e8bbe808bc1553a5b8079796fd5101c8fddcb815
Galina Mikhaylova <gala6858@mail.ru>
2024-01-04 15:10:06 +0300

    решение к домашнему заданию Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,25 @@
+"use strict";
+let arr1 = [];
+let arr2 = [];
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length != arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter((filt) => filt.gender === gender).map(year => year.age).reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length - 1) {
+      debugger;
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0);  
+  return result;
 }
